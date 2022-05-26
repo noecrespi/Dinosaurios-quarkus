a002e2170038e1b06238077eaa7177f04756e7f9
Noelia <ncrespipomar@cifpfbmoll.eu>
2022-05-26 17:17:18 +0200

    first commit

---
+++ src/test/java/edu/poniperro/quickstart/NativeDinoResourceIT.java
@@ -0,0 +1,9 @@
+package edu.poniperro.quickstart;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDinoResourceIT extends DinoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
